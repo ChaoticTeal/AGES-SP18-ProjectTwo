51c02ce63bd9fe3a7228770a04832c7a8679c240
PurpleNinjaLink <jawhittom@gmail.com>
2018-04-06 15:57:41 -0500

    Weekend Update 4/6
    Basic scene and movement implemented

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement: MonoBehaviour 
+{
+    // SerializeFields
+    [Tooltip("Player move speed.")]
+    [SerializeField]
+    float moveSpeed = 5f;
+
+    // Private fields
+    /// <summary>
+    /// Horizontal input check
+    /// </summary>
+    float horizontalInput;
+    /// <summary>
+    /// Vertical input check
+    /// </summary>
+    float verticalInput;
+    /// <summary>
+    /// Player Rigidbody2D
+    /// </summary>
+    Rigidbody2D rigidbody2D;
+
+	// Use this for initialization
+	void Start () 
+	{
+        rigidbody2D = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+        GetMoveInput();
+	}
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        rigidbody2D.velocity = new Vector2(horizontalInput, verticalInput) * moveSpeed;
+    }
+
+    private void GetMoveInput()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+    }
+}
